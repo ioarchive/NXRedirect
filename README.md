037cc3f6ae1f4fce1d5e7f9981245b3c755e741c
Animal <24845294+ItzOnlyAnimal@users.noreply.github.com>
2022-10-10 18:03:25 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,75 @@
+# NXRedirect
+
+A program to redirect/forward media tweets made from a Nintendo Switch onto Discord. 
+
+## Usage:
+
+1. Create a Twitter developer V2 app and grab the Bearer token
+   - Note: Private accounts are not supported right now
+2. Clone this repo somewhere
+
+
+Then create the following files:
+
+<details>
+<summary>config.json</summary>
+  
+```json
+{
+    "checkSource": true,
+    "autoReconnect": true,
+    "logDebug": false,
+    "token": "The bearer access token from your twitter app",
+    "webhookUrl": "Discord webhook url to send stuff into"
+}
+```
+`checkSource` will check the if the tweet source is tweeted from a switch ("Nintendo Switch Share")
+`autoReconnect` will automatically reconnect to twitter if it drops
+`logDebug` will log extra info like the tweet response to the console
+
+----
+</details>
+
+<details>
+<summary>rules.json</summary>
+
+`value`: The filters and keywords to check against, works similar to twitter search
+  - You can find a list of filter operators [here](https://developer.twitter.com/en/docs/twitter-api/tweets/search/integrate/build-a-query#list)
+  
+`tag`: Basically a display name for the value
+
+Example:
+```json
+[
+    {
+        "value": "from:SplatoonNA has:media",
+        "tag": "Media from Splatoon NA"
+    },
+    {
+        "value": "from:ProChara -is:retweet has:media",
+        "tag": "i am good at splatoon"
+    },
+    {
+        "value": "from:azulcrescent has:media",
+        "tag": "test tag"
+    }
+]
+```
+----
+</details>
+
+Finally, build and run:
+```
+pnpm i
+tsc
+node .
+```
+
+There is no feedback for successful connection, but it should be connected unless you receive an error.
+
+# Contributing
+To be completely honest I will probably not touch this for a while so if you wanna add something feel free to make a PR lol
+
+To do:
+- [ ] Add support for private accounts
+- [ ] Support removing added rules through same json file
---
+++ src/index.ts
@@ -0,0 +1,83 @@
+import { ETwitterStreamEvent, TwitterApi } from "twitter-api-v2";
+import config from "./config.json" assert { type: "json" };
+import rules from "./rules.json" assert { type: "json" };
+import { request, FormData } from "undici";
+import BodyReadable from "undici/types/readable";
+
+process.on("SIGINT", function () {
+    console.log("Closing stream and exiting");
+    stream.close();
+    process.exit();
+});
+
+const client = new TwitterApi(config.token);
+console.log("Starting twitter connection");
+
+const stream = await client.v2.searchStream();
+stream.autoReconnect = config.autoReconnect;
+
+
+stream.on(ETwitterStreamEvent.ConnectionError, e => console.log("Connection error:", e));
+stream.on(ETwitterStreamEvent.Error, e => console.log("Twitter error:", e));
+
+await client.v2.updateStreamRules({
+    add: [
+        ...rules
+    ]
+});
+for await (const { data } of stream) {
+    let tweet = await client.v1.singleTweet(data.id);
+    logDebug(tweet);
+    if (config.checkSource && !tweet.source.includes("Nintendo Switch Share")) {
+        console.log(`Tweet source of ${tweet.id_str} doesn't match, ignoring`);
+        continue;
+    }
+
+    let fileIndex = 0;
+    let formData = new FormData();
+
+    formData.append("payload_json", JSON.stringify({ content: (tweet.text || tweet.full_text)?.replace(/https:(\/\/t\.co\/([A-Za-z0-9]|[A-Za-z]){10})/, "") }));
+
+    for (let media of tweet.extended_entities?.media ?? []) {
+        if (media.type !== "video" && media.type !== "photo")
+            continue;
+        try {
+            let photoVideo: { type: string, data: BodyReadable };
+            switch (media.type) {
+                case "video":
+                    let bitrate = Math.max(...media.video_info!.variants.map(v => v.bitrate ?? 0));
+                    let variants = media.video_info!.variants.filter(v =>
+                        v.content_type == "video/mp4"
+                        && v.url !== undefined
+                        && v.bitrate == bitrate);
+                    console.log("Downloading video", variants[0].url);
+                    photoVideo = { type: "mp4", data: await request(variants[0].url).then(r => r.body), };
+                    break;
+                case "photo":
+                    console.log("Downloading photo", media.media_url_https);
+                    photoVideo = { type: "jpg", data: await request(media.media_url_https).then(r => r.body) };
+                    break;
+                default:
+                    throw new Error("Unknown media type");
+            }
+            formData.append(`file${fileIndex.toString()}`, await photoVideo.data.blob(), `nxr_${tweet.id_str}.${photoVideo.type}`);
+        } catch (e) {
+            console.log(e);
+        }
+        fileIndex++;
+    }
+
+    logDebug(formData)
+    request(config.webhookUrl, {
+        method: "POST",
+        body: formData
+    });
+}
+
+function logDebug(info: any, ...args: any) {
+    if (config.logDebug)
+        console.log(info, args);
+}
+
+// TODO add authenticated users eventually
+
